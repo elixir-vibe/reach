118f23224cd169cc4b9c029f470af7c3637939a8
Danila Poyarkov <dev@dannote.net>
2026-07-17 19:24:38 +0200

    Support Elixir 1.18

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Reach.MixProject do
     [
       app: :reach,
       version: @version,
-      elixir: "~> 1.19",
+      elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps(),
